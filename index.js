f7dad8ebfa0232e5cacfaad7368a24a2e388e2db
Danail Nachev <dnachev@linkedin.com>
2018-07-21 09:29:02 -0700

    Allow to configure which files to be moved to vendor static

--- index.js
+++ index.js
@@ -1,68 +1,89 @@
 /* eslint-env node */
-"use strict";
+'use strict';
 
 const fs = require('fs');
 const vendorStaticFilepath = 'assets/vendor-static.js';
 const vendorFilepath = '/assets/vendor.js';
 
 module.exports = {
-  name: "ember-vendor-split",
+  name: 'ember-vendor-split',
   included(app) {
-    let jqueryPath, emberProdPath, emberDebugPath;
-
     const hasBower = fs.existsSync(app.root + '/bower.json');
     const emberSource = app.project.findAddonByName('ember-source');
 
+    let filesConfig = ((app.options['ember-vendor-split'] || {}).files) || [];
+
     const useSource = !hasBower && emberSource;
-    removeOutputFiles(app, useSource, emberSource);
 
     if (useSource) {
-      jqueryPath = emberSource.paths.jquery
-      emberProdPath = emberSource.paths.prod
-      emberDebugPath = emberSource.paths.debug
+      filesConfig = [
+        ...filesConfig,
+        emberSource.paths.jquery,
+        {
+          production: emberSource.paths.prod,
+          development: emberSource.paths.debug,
+        },
+      ];
     } else {
-      jqueryPath = `${app.bowerDirectory}/jquery/dist/jquery.js`;
-      emberProdPath = `${app.bowerDirectory}/ember/ember.prod.js`;
-      emberDebugPath = `${app.bowerDirectory}/ember/ember.debug.js`;
+      filesConfig = [
+        ...filesConfig,
+        `${app.bowerDirectory}/jquery/dist/jquery.js`,
+        {
+          production: `${app.bowerDirectory}/ember/ember.prod.js`,
+          development: `${app.bowerDirectory}/ember/ember.debug.js`,
+        },
+      ];
     }
 
-    app.import(jqueryPath, {
-      outputFile: vendorStaticFilepath
-    });
+    const filesToRemove = filesConfig.reduce((result, file) => {
+      if (typeof file === 'string') {
+        result.push(file);
+      } else {
+        if (file.development) {
+          result.push(file.development);
+        }
+        if (file.production) {
+          result.push(file.production);
+        }
+      }
+      return result;
+    }, []);
+    removeOutputFiles(app, filesToRemove);
 
-    app.import({
-      development: emberDebugPath,
-      production: emberProdPath
-    }, {
-      outputFile: vendorStaticFilepath
+    filesConfig.forEach(fileConfig => {
+      const options = {
+        outputFile: vendorStaticFilepath,
+      };
+      if (typeof fileConfig === 'string') {
+        app.import(fileConfig, options);
+      } else {
+        const importFiles = {};
+        if (fileConfig.production !== undefined) {
+          importFiles.production = fileConfig.production;
+        }
+        if (fileConfig.development !== undefined) {
+          importFiles.development = fileConfig.development;
+        }
+        if (fileConfig.prepend !== undefined) {
+          options.prepend = fileConfig.prepend;
+        }
+        app.import(importFiles, options);
+      }
     });
   },
   updateFastBootManifest: function(manifest) {
     manifest.vendorFiles.unshift(vendorStaticFilepath);
     return manifest;
-  }
+  },
 };
 
 module.exports.removeOutputFiles = removeOutputFiles;
-function removeOutputFiles(app, useSource, emberSource) {
+function removeOutputFiles(app, filesToRemove) {
   // TODO: public API for ember-cli? maybe: https://github.com/ember-cli/ember-cli/pull/7060
-  let filesToRemove;
-  if (useSource) {
-    filesToRemove = [
-      emberSource.paths.jquery,
-      emberSource.paths.prod,
-      emberSource.paths.debug
-    ];
-  } else {
-    filesToRemove = [
-      `${app.bowerDirectory}/jquery/dist/jquery.js`,
-      `${app.bowerDirectory}/ember/ember.prod.js`,
-      `${app.bowerDirectory}/ember/ember.debug.js`
-    ];
-  }
-
   for (let i = 0; i < filesToRemove.length; i++) {
-    let index = app._scriptOutputFiles[vendorFilepath].indexOf(filesToRemove[i]);
+    let index = app._scriptOutputFiles[vendorFilepath].indexOf(
+      filesToRemove[i],
+    );
     if (index > -1) {
       app._scriptOutputFiles[vendorFilepath].splice(index, 1);
     }
